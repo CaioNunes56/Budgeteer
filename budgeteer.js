54940c2297577938742524ae27683e9f5a0dd45b
CaioNunes56 <caio.pinheiro.nunes@gmail.com>
2025-04-27 07:13:16 -0400

    UpdatedApiKey

--- budgeteer.js
+++ budgeteer.js
@@ -58,13 +58,13 @@ document.addEventListener('DOMContentLoaded', () => {
     For each category, provide a bool value indicating if the user is managing his money well (true) or if it needs some improvement(false) (the value should be under the header)
     For each category where the bool is true, Explain why they are doing good and give any relevant advice. If the bool is false, suggest important changes the user should make.`;
 
-    const apiURL = 'https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash-001:generateContent?key=AIzaSyD48msYI5xDeKxlPKSrwBMtpkaRIZH77IM';
+    const apiURL = 'https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash-001:generateContent?key=AIzaSyAUff-DpCq92IKN8AnVm0wdyKMycKwOauk';
 
     const dataSent = {
       "contents": [{ "parts": [{ "text": stringToSend}] }]
     };
 
-    const listModelsURL = 'https://generativelanguage.googleapis.com/v1/models?key=AIzaSyD48msYI5xDeKxlPKSrwBMtpkaRIZH77IM';
+    const listModelsURL = 'https://generativelanguage.googleapis.com/v1/models?key=AIzaSyAUff-DpCq92IKN8AnVm0wdyKMycKwOauk';
 
     const jsonstring = JSON.stringify(dataSent);
 
